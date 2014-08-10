5a76e14ba6be4fdffbd5b1a0f83a94de5afae554
Vicky <viikadrug@Vickys-MacBook-Pro.local>
2014-08-10 12:42:47 +0400

    added slider to pages

--- js/main.js
+++ js/main.js
@@ -4,4 +4,31 @@
 
 $(document).on('ready', function(){
     $('.no-placeholder input[placeholder], .no-placeholder textarea[placeholder]').placeholder();
+
+    $(function() {
+        $('.jcarousel').jcarousel({
+            'wrap': 'circular',
+            animation: {
+                duration: 1000,
+                complete: function(){
+                    setTimeout(function(){console.log('123')},500)
+                },
+            }
+        });
+    });
+
+    $('.jcarousel-pagination').jcarouselPagination({
+        item: function(page) {
+            return '<a href="#' + page + '" onClick="return false;"></a>';                    }
+    });
+
+    $('.jcarousel').jcarousel({
+    })
+        .jcarouselAutoscroll({
+            interval: 3000,
+            target: '+=1',
+            autostart: true
+        })
+    ;
+
 });
